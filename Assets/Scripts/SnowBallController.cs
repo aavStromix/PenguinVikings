a2b9df28dafe127a7f295ab1cb6cf8ef879864bf
aavStromix <aav_work@mail.ru>
2015-12-06 23:19:25 +0600

    "Throw Snowball" Animation
    Set "Throw Snowball" Animation to Player

--- Assets/Scripts/SnowBallController.cs
+++ Assets/Scripts/SnowBallController.cs
@@ -7,17 +7,22 @@ public class SnowBallController : MonoBehaviour {
 	public float speed = 30f;
 
 	private PlayerController playerCtrl;
+	private Animator anim;
+
 	// Use this for initialization
 	void Start () {
 		playerCtrl = transform.root.GetComponent<PlayerController>();
+		anim = transform.root.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown("Fire1"))
-		{
-			Rigidbody2D bulletInstance = Instantiate(snowball, transform.position, Quaternion.Euler(playerCtrl.mainVector.normalized)) as Rigidbody2D;
-			bulletInstance.AddForce(playerCtrl.mainVector.normalized * speed);
+		if (Input.GetButtonDown ("Fire1")) {
+			anim.SetBool ("isThrowing", true);
+			Rigidbody2D bulletInstance = Instantiate (snowball, transform.position, Quaternion.Euler (playerCtrl.mainVector.normalized)) as Rigidbody2D;
+			bulletInstance.AddForce (playerCtrl.mainVector.normalized * speed);
+		} else {
+			anim.SetBool ("isThrowing", false);
 		}
 	}
 }
